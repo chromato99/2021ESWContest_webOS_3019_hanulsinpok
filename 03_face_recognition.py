6184b7f10aa880b31f9f7bf28c5a434c35c0d789
임시우 (Siwoo Lim) <siwoo5530@gmail.com>
2021-08-31 22:53:58 +0900

    FaceRecognition
    need webcam

---
+++ 03_face_recognition.py
@@ -0,0 +1,70 @@
+import cv2
+import numpy as np
+import os
+import requests
+
+url = "file:/home/pi/project/websockets/webClient.html"
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read('trainer/trainer.yml')
+cascadePath = "Cascades/haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascadePath);
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+#iniciate id counter
+id = 0
+
+# names related to ids: example ==> loze: id=1,  etc
+# 이런식으로 사용자의 이름을 사용자 수만큼 추가해준다.
+names = ['None', 'siwoo', 'bumki', 'seulwon', 'dahae']
+
+# Initialize and start realtime video capture
+cam = cv2.VideoCapture(0)
+cam.set(3, 640) # set video widht
+cam.set(4, 480) # set video height
+
+# Define min window size to be recognized as a face
+minW = 0.1*cam.get(3)
+minH = 0.1*cam.get(4)
+
+while True:
+    ret, img =cam.read()
+    img = cv2.flip(img, 1) # Flip vertically
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    
+    faces = faceCascade.detectMultiScale( 
+        gray,
+        scaleFactor = 1.2,
+        minNeighbors = 5,
+        minSize = (int(minW), int(minH)),
+       )
+
+    for(x,y,w,h) in faces:
+        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+        id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
+        # Check if confidence is less them 100 ==> "0" is perfect match
+        if (confidence < 100):
+            id = names[id] #특정 사용자를 인식했을 때 라즈베리 파이에 어떤 이벤트를 발생시키고 싶다면 아랫부분에 코드를 추가해주자
+            confidence = "  {0}%".format(round(100 - confidence)) # 이부분에서 if제어문 등으로 id가 특정 값일 때 원하는 이벤트를 발생시키면 된다.
+            if id == 'siwoo':
+                name = id
+                print(name)
+                #response = requests.post(url, data = name)
+                
+        else:
+            id = "unknown"
+            confidence = "  {0}%".format(round(100 - confidence))
+        
+        cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
+        cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
+    
+    cv2.imshow('camera',img) 
+    k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
+    if k == 27:
+        break
+
+# Do a bit of cleanup
+print("\n [INFO] Exiting Program and cleanup stuff")
+cam.release()
+
+cv2.destroyAllWindows()
