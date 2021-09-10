4cca3ae3dfe8278e336d6015203602df8a91c8a9
BeomgiKim <chromato99@gmail.com>
2021-09-10 21:59:56 +0900

    delete wrong function call

--- car2home/hass.js
+++ car2home/hass.js
@@ -5,15 +5,13 @@ import {
     createLongLivedTokenAuth,
 } from "./lib/index.js";
 
-const token = require('./token.json');
+// const token = require('./token.json');
 (async () => {
 const auth = createLongLivedTokenAuth(
     "http://192.168.219.108:8123",
-    token.access_token
+    ""
 );
 
 const connection = await createConnection({ auth });
 subscribeEntities(connection, (entities) => console.log(entities));
 })();
-
-connect();
